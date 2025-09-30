9d44f4877005c2113989eb5a850adf8982c4785a
Muhammad-Umar-406 <heymuhammadumar406@gmail.com>
2025-09-30 19:30:38 +0500

    updated javascript

--- app.js
+++ app.js
@@ -1,40 +1,10 @@
-// let hr = document.getElementById("di_hr");
-// let min = document.getElementById("di_min");
-// let sec = document.getElementById("di_sec");
-
-// function updateclock() {
-//   let date = new Date();
-//   let hrs = date.getHours();
-//   let mins = date.getMinutes();
-//   let secs = date.getSeconds();
-//   hr.innerHTML = hrs;
-//   min.innerHTML = mins;
-//   sec.innerHTML = secs;
-// }
-// setInterval(updateclock, 1000);
-// updateclock();
-
-// function changingFormat(){
-//   let date = new Date();
-//   let hrs = date.getHours();
-//   let mins = date.getMinutes();
-//   let secs = date.getSeconds();
-//   if(date - 12){
-    
-//   }
-
-
-// }
-
-
-
 let hr = document.getElementById("di_hr");
 let min = document.getElementById("di_min");
 let sec = document.getElementById("di_sec");
 let convertingPmAm = document.getElementById("ampm");
 let button = document.getElementById("button");
 
-let is12Hour = true; // default 12-hour format
+let is12Hour = true; 
 
 function updateClock() {
     let date = new Date();
@@ -49,7 +19,7 @@ function updateClock() {
         if (hrs === 0) hrs = 12;
         convertingPmAm.textContent = ampm;
     } else {
-        convertingPmAm.textContent = ""; // 24-hour me AM/PM hide
+        convertingPmAm.textContent = ""; 
     }
 
     hr.textContent = hrs.toString().padStart(2, "0");
@@ -60,7 +30,6 @@ function updateClock() {
 setInterval(updateClock, 1000);
 updateClock();
 
-// simple function jo HTML se call hoga
 function changingFormat() {
     is12Hour = !is12Hour;
     button.textContent = is12Hour ? "Switch to 24 hrs" : "Switch to 12 hrs";
